4070449a3243328d347c976d48700d75f7234f0e
tochkar <tochkaralisa@gmail.com>
2020-10-26 10:50:18 +0300

    playground

---
+++ Playground.h
@@ -0,0 +1,20 @@
+
+
+#ifndef AL_PLAYGROUND_H
+#define AL_PLAYGROUND_H
+
+#include <string>
+
+using namespace std;
+
+class Playground{
+public:
+    string gameField [10][10];
+
+    void inicializationOfGameField();
+
+    Playground();
+};
+
+
+#endif //AL_PLAYGROUND_H
